f08610ec8d8e2b57c8313b4187ead22466529a15
Joseph Hirschfeld <joe@ibj.io>
2015-12-17 19:21:04 -0500

    Use websockets for pinging

--- public/js/ping.js
+++ public/js/ping.js
@@ -1,35 +1,31 @@
-window.onload = function() {
+window.onload = function () {
     var pingElement = document.getElementById("ping");
     var colorChanger = document.getElementById("pingIndicator");
-    var scale = chroma.scale(['green','yellow','red']);
-    var runIt = function() {
-        var req = new XMLHttpRequest();
-        req.open('get','/p?r='+Math.random(),true);
-        req.onreadystatechange = function() {
-            if(req.readyState == 4){
-                if(req.status != 200){
-                    pingelement.innerHTML = '&infin;';
-                    colorChanger.style.color = '#000000';
-                    schedule();
-                } else {
-                    var diff = new Date().getTime() - time;
-                    pingElement.innerHTML = diff;
-                    var spread = diff / 600;
-                    if(spread > 1){
-                        spread = 1;
-                    }
-                    colorChanger.style.color = scale(spread);
-                    schedule()
-                }
-            }
-        };
-        var time = new Date().getTime();
-        req.send();
-    };
-
-    var schedule = function(){
+    var scale = chroma.scale(['green', 'yellow', 'red']);
+    var socket = new WebSocket('wss://ibj.io','ping');
+    var schedule = function () {
         setTimeout(runIt, 1000);
     };
+    var runIt = function () {
+        socket.send('ping');
+        var time = new Date().getTime();
+        socket.onmessage = function (event) {
+            if (event.data != 'pong') {
+                socket.close();
+                return;
+            }
+            var diff = new Date().getTime() - time;
+            pingElement.innerHTML = diff;
+            var spread = diff / 600;
+            if (spread > 1) {
+                spread = 1;
+            }
+            colorChanger.style.color = scale(spread);
+            schedule()
+        };
+    };
 
-    schedule();
+
+    socket.onopen = schedule;
+
 };
---
+++ websocket.js
@@ -0,0 +1,27 @@
+var websocket = require('websocket').server;
+
+module.exports = function (server) {
+    var sock = new websocket({
+        httpServer: server,
+        autoAcceptConnections: false
+    });
+
+    sock.on('request', function (request) {
+        if (!originIsAllowed(request.origin)) {
+            return request.reject();
+        }
+
+        var connection = request.accept('ping', request.origin);
+        connection.on('message', function (msg) {
+            if (msg.type == 'utf8') {
+                if (msg.utf8Data == 'ping') {
+                    connection.sendUTF('pong');
+                } else {
+                    connection.close();
+                }
+            } else {
+                connection.close();
+            }
+        })
+    });
+};
